7823ecafdcf0a1a699df71fb1d4956bd835bb6e3
AdiminBlueStep <164308569+AdiminBlueStep@users.noreply.github.com>
2026-08-08 23:29:41 +0900

    Create design4.v

---
+++ 01_4bit_adder/design4.v
@@ -0,0 +1,14 @@
+// Code your design here
+module  counter_4bit (
+  input wire	clk,
+  input wire	rst,
+  output reg[3:0] q
+);
+  always @(posedge clk)begin
+    if (rst) begin
+      q<= 4'b0000;
+    end else begin
+      q <= q +1'b1;
+    end
+  end
+endmodule
