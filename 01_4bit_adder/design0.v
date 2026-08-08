1585c3710e07d7635980581296b0282aa8f29d4f
AdiminBlueStep <164308569+AdiminBlueStep@users.noreply.github.com>
2026-08-08 22:46:01 +0900

    Create design0.v

---
+++ 01_4bit_adder/design0.v
@@ -0,0 +1,13 @@
+//이번 과제에선 가장 기본적인 베릴로그 작성 양식을 기술한다.
+/* 모듈 선언부 */
+// 모듈의 이름, 입출력 포트 , parameter 등 선언함 //
+module my_and_module (a, b, c);
+    /* 신호 선언부 */
+  // wire, reg,, parameter 등 선언함
+  inpuut wire a, b;
+  output wire c;
+
+  /* 동작 선언부 */
+  // assign, always, initial, 하위 모듈 인스턴스 등 선언함
+  assign c = a & b;
+endmodule
